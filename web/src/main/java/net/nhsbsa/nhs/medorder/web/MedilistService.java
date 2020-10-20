4a0a2927727b174857c3d9a017d808b31b246318
Kieran West <KIEWE@nhsbsaadminsmbp.home>
2020-10-20 11:35:47 +0100

    adding db connection pages

---
+++ web/src/main/java/net/nhsbsa/nhs/medorder/web/MedilistService.java
@@ -0,0 +1,32 @@
+package net.nhsbsa.nhs.medorder.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@Service
+public class MedilistService {
+    @Autowired
+    private Medlistrepo medlisrepo;
+
+     @RequestMapping("/index")
+    public List<medlist> getAllmeds() {
+            List<medlist> medi = new ArrayList<>();
+            medlisrepo.findAll()
+                    .forEach(medi :: add);
+            return medi;
+        }
+        @PostMapping("/index")
+    public void addMeds(@RequestBody medlist mediname){
+        medlisrepo.save(mediname);
+    }
+
+
+
+}
--- web/src/main/java/net/nhsbsa/nhs/medorder/web/medlist.java
+++ web/src/main/java/net/nhsbsa/nhs/medorder/web/medlist.java
@@ -20,13 +20,22 @@ public medlist(String id, String mediname){
 public String getId(){
         return id;
 }
+<<<<<<< Updated upstream
 public void setId(String id){
+=======
+    public void setId(final long id){
+>>>>>>> Stashed changes
         this.id=id;
 }
 public String getMediname(){
+<<<<<<< Updated upstream
         return Mediname; }
 
 public void setMediname(String Mediname){
+=======
+        return  this.Mediname; }
+        public void setMediname(final String Mediname){
+>>>>>>> Stashed changes
         this.Mediname=Mediname;
     }
 }
